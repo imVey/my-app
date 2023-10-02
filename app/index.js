4483f4cc5bbc387bbc739593cdb995193e7219f1
imVey <marwanbella91@hotmail.fr>
2023-10-02 09:11:16 +0200

    First commit

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>hello world</Text>
+        </View>
+    )
+}
+
+export default Home;
